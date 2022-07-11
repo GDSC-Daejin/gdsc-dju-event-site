239030bb412409029babb83e0879fa846381dc70
HyeokE <jhjeong00@gmail.com>
2022-07-11 17:26:42 +0900

    chore: modify footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -13,11 +13,15 @@ export const Footer: React.FC<{ disable?: boolean }> = ({ disable = true }) => {
         <FooterText google={true} href={'https://developers.google.com/'}>
           Google
         </FooterText>
-
         <FooterText href={'https://developers.google.com/community-guidelines'}>
           Community guidelines
+        </FooterText>{' '}
+        <FooterText href={'https://gdsc-dju-blog.web.app/'}>
+          GDSC DJU Tech Blog
         </FooterText>
-        <FooterText href={'/faq'}>FAQ</FooterText>
+        <FooterText href={'https://gdsc-dju.web.app/'}>
+          GDSC DJU Site
+        </FooterText>
       </FooterLogo>
       <FooterCopyRight>{'Copyright © '}GDSC Daejin 2021</FooterCopyRight>
     </FooterWrapper>
